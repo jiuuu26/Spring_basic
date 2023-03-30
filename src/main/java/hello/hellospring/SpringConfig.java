18e1f3b79ed6803b7f8955885c7d017c1c57b13b
jiuuu26 <wldn4036@naver.com>
2023-03-31 01:03:28 +0900

    section 6

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,8 +1,6 @@
 package hello.hellospring;
 
-import hello.hellospring.domain.Member;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,14 +8,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
-    @Bean
-    public MemberService memberService(){
-        return new MemberService(memberRepository());
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+    public MemberService memberService(){
+        return new MemberService(memberRepository);
     }
+
+    /*@Bean
+    public MemberRepository memberRepository(){
+        //return new MemoryMemberRepository();
+       // return new JdbcMemberRepository(datasource);
+      //  return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
+    }*/
 }
 
--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,6 +1,13 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.*;
+
+@Entity
 public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
